dbd01a8f5986f6b721b6aba6e9f6277c84f0a3e0
Rob <hello@robmcdonald.me>
2025-06-15 21:52:20 +0100

    Beginning of the user auth guide

--- 04-user-auth/src/Pages/SignIn.elm
+++ 04-user-auth/src/Pages/SignIn.elm
@@ -2,7 +2,9 @@ module Pages.SignIn exposing (Model, Msg, page)
 
 import Effect exposing (Effect)
 import Route exposing (Route)
-import Html
+import Html exposing (Html)
+import Html.Attributes as Attr
+import Html.Events
 import Page exposing (Page)
 import Shared
 import View exposing (View)
@@ -23,12 +25,18 @@ page shared route =
 
 
 type alias Model =
-    {}
+    { email : String
+    , password : String
+    , isSubmittingForm : Bool
+    }
 
 
 init : () -> ( Model, Effect Msg )
 init () =
-    ( {}
+    ( { email = ""
+      , password = ""
+      , isSubmittingForm = False
+      }
     , Effect.none
     )
 
@@ -38,14 +46,29 @@ init () =
 
 
 type Msg
-    = NoOp
+    = UserUpdatedInput Field String
+    | UserSubmittedForm
+
+type Field
+    = Email
+    | Password
 
 
 update : Msg -> Model -> ( Model, Effect Msg )
 update msg model =
     case msg of
-        NoOp ->
-            ( model
+        UserUpdatedInput Email value ->
+            ( { model | email = value }
+            , Effect.none
+            )
+        
+        UserUpdatedInput Password value ->
+            ( { model | password = value }
+            , Effect.none
+            )
+        
+        UserSubmittedForm ->
+            ( { model | isSubmittingForm = True }
             , Effect.none
             )
 
@@ -66,5 +89,79 @@ subscriptions model =
 view : Model -> View Msg
 view model =
     { title = "Pages.SignIn"
-    , body = [ Html.text "/sign-in" ]
+    , body = 
+        [ viewPage model
+        ]
     }
+
+viewPage : Model -> Html Msg
+viewPage model =
+    Html.div [ Attr.class "columns is-mobile is-centered" ]
+        [ Html.div [ Attr.class "column is-narrow" ]
+            [ Html.div [ Attr.class "section" ]
+                [ Html.h1 [ Attr.class "title" ] [ Html.text "Sign in" ]
+                , viewForm model
+                ]
+            ]
+        ]
+
+viewForm : Model -> Html Msg
+viewForm model =
+    Html.form [ Attr.class "box", Html.Events.onSubmit UserSubmittedForm ]
+        [ viewFormInput
+            { field = Email
+            , value = model.email
+            }
+        , viewFormInput
+            { field = Password
+            , value = model.password
+            }
+        , viewFormControls model
+        ]
+
+viewFormInput : { field : Field, value : String } -> Html Msg
+viewFormInput options =
+    Html.div [ Attr.class "field" ]
+        [ Html.label [ Attr.class "label" ] [ Html.text (fromFieldToLabel options.field) ]
+        , Html.div [ Attr.class "control" ]
+            [ Html.input
+                [ Attr.class "input"
+                , Attr.type_ (fromFieldToInputType options.field)
+                , Attr.value options.value
+                , Html.Events.onInput (UserUpdatedInput options.field)
+                ]
+                []
+            ]
+        ]
+
+fromFieldToLabel : Field -> String
+fromFieldToLabel field =
+    case field of
+        Email ->
+            "Email address"
+        
+        Password ->
+            "Password"
+
+fromFieldToInputType : Field -> String
+fromFieldToInputType field =
+    case field of
+        Email ->
+            "email"
+        
+        Password ->
+            "password"
+
+viewFormControls : Model -> Html Msg
+viewFormControls model =
+    Html.div [ Attr.class "field is-grouped is-grouped-right" ]
+        [ Html.div
+            [ Attr.class "control" ]
+            [ Html.button
+                [ Attr.class "button is-link"
+                , Attr.disabled model.isSubmittingForm
+                , Attr.classList [ ("is-loading", model.isSubmittingForm) ]
+                ]
+                [ Html.text "Sign in" ]
+            ]
+        ]
